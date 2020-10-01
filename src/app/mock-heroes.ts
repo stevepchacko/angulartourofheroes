9e8fc79f823a68d6ddbd3973953e17ae27802580
Steve Chacko <schacko@orthofx.com>
2020-10-01 10:44:17 +0530

    added hero details component

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,9 @@
+ import { Hero } from './hero';
+
+ export const HEROES: Hero[]= [
+     {id:11, name: 'Dr Nice'},
+     {id:12, name: 'Dr bice'},
+     {id:13, name: 'Dr lice'},
+     {id:14, name: 'Dynamo'},
+     {id:15, name: 'Mr Incredible'},
+ ];
